fefb24ff130ada56c67829e17984dd1a490886d9
collin4210 <collinsasuke@gmail.com>
2022-01-12 17:48:05 +0100

    Erste Funktionen : FileReader erstellt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const PORT = process.env.port || 6969;
 const api = express();
+const readfile = require('./fileReader.js');
 
 api.listen(PORT, () => {
     console.log("API läuft!")
@@ -19,11 +20,12 @@ api.post('/add', (req, res) => {
 */
 
 
-api.get('/wg', (req, res) => {
+api.get('/produkt/id', (req, res) => {
    
     res.status(200).send({
-        WG: 'TestWG1'
-
+        
+        
 
     })
 });
+
