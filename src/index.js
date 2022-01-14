05d352d6846708ec48315280861554e929b12a58
collin4210 <collinsasuke@gmail.com>
2022-01-14 01:28:09 +0100

    file reader changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const PORT = process.env.port || 6969;
 const api = express();
 const readfile = require('./fileReader.js');
+const { fstat } = require("fs")
+const fs = require('fs');
+const { arrayBuffer, json } = require("stream/consumers");
+const produktePath = './src/Data/produkte.json';
 
 api.listen(PORT, () => {
     console.log("API läuft!")
@@ -12,20 +16,52 @@ api.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-var data = readfile.readData('./src/Data/produkte.json');
-api.get("/produkt/:ID", (req, res) => {
-    
-    res.send(data());
-  
-});
-
-api.post("/produkt/", (req,res) => {
-    var edit = readfile.writeData('./src/Data/produkte.json')
-    //Wie?
-
-});
 
 api.get("/wg", (req, res) => {
-    
+
     res.status(200).send(data(console.log));
 });
+
+
+fs.readFile(produktePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return
+    }
+
+    var produkte = JSON.parse(data);
+    api.get("/produkt/:ID", (req, res) => {
+
+        res.send(produkte[req.params.ID - 1]);
+    });
+
+
+    api.get("/produkt", (req, res) => {
+
+        res.send(produkte);
+    });
+
+    api.post("/produkt", (req, res) => {
+
+        
+        var newData = JSON.stringify(produkte);
+        
+
+        fs.writeFile(produktePath, newData, 'utf8', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+            
+        });
+   
+        res.send(console.log(produkte));
+
+    });
+
+});
+
+
+
+
+
