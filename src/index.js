1190480d62eee056c58cd0ed6e6a7f1ab423e40d
collin4210 <collinsasuke@gmail.com>
2022-01-14 16:01:39 +0100

    HTTP Codes eingefügt

--- src/index.js
+++ src/index.js
@@ -26,19 +26,29 @@ fs.readFile(WgPath, 'utf8', (err, data) => {
     let Wgs = JSON.parse(data);
 
     api.get("/wg/:wgID/shoppinglist/:productID", (req, res) => {
+        let thisWG = Wgs.find(wg => wg.id === parseInt(req.params.wgID));
+        if(!thisWG) res.status(404).send('WG not found');
 
-        res.status(200).send(Wgs[req.params.wgID - 1].ShoppingList[req.params.productID-1]);
+        let thisProduct = thisWG.ShoppingList.find(list => list.ProductID === parseInt(req.params.productID));
+        if(!thisProduct) res.status(404).send('Product not found');
+
+
+        res.status(200).send(thisProduct);
     });
 
 
     api.get("/wg/:ID/shoppinglist", (req, res) => {
-
-        res.status(200).send(Wgs[req.params.ID - 1].ShoppingList);
+        let thisWG = Wgs.find(wg => wg.ID == parseInt(req.params.ID));
+        if(!thisWG) res.status(404).send('WG not found');
+        res.status(200).send(thisWG.ShoppingList);
     });
 
     api.get("/wg/:ID", (req, res) => {
-
-        res.status(200).send(Wgs[req.params.ID - 1]);
+     
+        let thisWG = Wgs.find(wg => wg.ID == parseInt(req.params.ID));
+        
+        if(!thisWG) res.status(404).send('WG not found');
+        res.status(200).send(thisWG);
     });
 
     api.get("/wg", (req, res) => {
@@ -50,9 +60,9 @@ fs.readFile(WgPath, 'utf8', (err, data) => {
 
 
         const newWG = {
-            ID : Wgs.length + 1,
-            PersonCount : req.body.PersonCount,
-            Price  : 0,
+            ID: Wgs.length + 1,
+            PersonCount: req.body.PersonCount,
+            Price: 0,
             PricePerPerson: 0,
 
         }
@@ -64,10 +74,15 @@ fs.readFile(WgPath, 'utf8', (err, data) => {
         fs.writeFile(WgPath, newData, 'utf8', function (err) {
             if (err) {
                 return console.log(err);
+                res.code(500);            
             }
             console.log("The file was saved!");
+            res.status(201).send("created successfully!");
+
+
 
         });
+
 
         res.status(201).send(console.log(Wgs));
 
@@ -77,27 +92,36 @@ fs.readFile(WgPath, 'utf8', (err, data) => {
 
 
         const newProduct = {
-            ProductID: Wgs[req.params.wgID-1].ShoppingList.length+1 ,
+            ProductID: Wgs[req.params.wgID - 1].ShoppingList.length + 1,
             Product: req.body.ProductName,
             Price: req.body.Price
         }
 
-        Wgs[req.params.wgID-1].ShoppingList.push(newProduct);
+        Wgs[req.params.wgID - 1].ShoppingList.push(newProduct);
+        Wgs[req.params.wgID - 1].ShoppingList.forEach(element => {
+            Wgs[req.params.wgID - 1].Price = Wgs[req.params.wgID - 1].Price + element.Price;
+        });
+        Wgs[req.params.wgID - 1].PricePerPerson = Wgs[req.params.wgID - 1].Price / Wgs[req.params.wgID - 1].PersonCount;
 
         newData = JSON.stringify(Wgs);
 
         fs.writeFile(WgPath, newData, 'utf8', function (err) {
             if (err) {
                 return console.log(err);
-                res.code(500); 
+                res.code(500);            
             }
             console.log("The file was saved!");
             res.status(201).send("created successfully!");
 
+
+
         });
 
-      
+
     });
+
+
+    
 
 });
 
