19521f47d90724dba4a6c34263b9984734c54d26
collin4210 <collinsasuke@gmail.com>
2022-01-14 11:51:09 +0100

    wg get

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const PORT = process.env.port || 6969;
 const api = express();
-const readfile = require('./fileReader.js');
 const { fstat } = require("fs")
 const fs = require('fs');
 const { arrayBuffer, json } = require("stream/consumers");
-const produktePath = './src/Data/produkte.json';
+const WgPath = './src/Data/wgs.json';
+
+api.use(express.json());
 
 api.listen(PORT, () => {
     console.log("API läuft!")
@@ -16,45 +17,58 @@ api.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-
-api.get("/wg", (req, res) => {
-
-    res.status(200).send(data(console.log));
-});
-
-
-fs.readFile(produktePath, 'utf8', (err, data) => {
+fs.readFile(WgPath, 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return
     }
 
-    var produkte = JSON.parse(data);
-    api.get("/produkt/:ID", (req, res) => {
+    let Wgs = JSON.parse(data);
 
-        res.send(produkte[req.params.ID - 1]);
+    api.get("/wg/:wgID/shoppinglist/:productID", (req, res) => {
+
+        res.status(200).send(Wgs[req.params.wgID - 1].ShoppingList[req.params.productID-1]);
     });
 
 
-    api.get("/produkt", (req, res) => {
+    api.get("/wg/:ID/shoppinglist", (req, res) => {
 
-        res.send(produkte);
+        res.status(200).send(Wgs[req.params.ID - 1].ShoppingList);
     });
 
-    api.post("/produkt", (req, res) => {
+    api.get("/wg/:ID", (req, res) => {
 
-        
-        var newData = JSON.stringify(produkte);
-        
+        res.status(200).send(Wgs[req.params.ID - 1]);
+    });
 
-        fs.writeFile(produktePath, newData, 'utf8', function (err) {
+    api.get("/wg", (req, res) => {
+
+        res.status(200).send(Wgs);
+    });
+
+    api.post("/wg", (req, res) => {
+
+
+        const newWG = {
+            id : Wgs.length + 1,
+            PersonCount : req.body.PersonCount,
+            Price  : 0,
+            PricePerPerson: 0,
+
+        }
+
+        Wgs.push(newWg);
+
+        newData = JSON.stringify(Wgs);
+
+        fs.writeFile(WgPath, newData, 'utf8', function (err) {
             if (err) {
                 return console.log(err);
             }
             console.log("The file was saved!");
-            
+
         });
-   
+
         res.send(console.log(produkte));
 
     });
